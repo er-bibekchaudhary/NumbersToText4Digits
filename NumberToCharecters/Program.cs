1e5d07408c0d9fc17f3707aa0fe0580c212265b5
Bibek Chaudhary <Mail2bibek@gmail.com>
2014-06-09 09:18:51 +0545

    Adding Processes to additional Library

--- NumberToCharecters/Program.cs
+++ NumberToCharecters/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NumberToText;
 
 namespace NumberToCharecters
 {
@@ -9,53 +10,31 @@ namespace NumberToCharecters
     {
         static void Main(string[] args)
         {
-            int PlaceValue, Numb;
-
-            string[] Numbers = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Elevan", "Twelve", "Thirteen",
-                                   "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eightteen", "Nineteen" };
-            
-            string[] Tens = { "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninty" };
-
-            Console.WriteLine("Enter a Number Between 1 to 9999");
-            Numb = int.Parse(Console.ReadLine());
-
-            //for Thousand Place Value
-            PlaceValue = Numb / 1000;
-            if(PlaceValue>=1 && PlaceValue<=9)
+            string choi=" ";
+            while (choi != "E")
             {
-                Console.Write(Numbers[PlaceValue - 1] + " Thousand ");
-            }
+                ToText.DigitsToText();
+                Console.WriteLine("\n\nPress E to End or C To Continue");
+                choi = Console.ReadLine();
+                
+                if (choi == "c")
+                    choi = "C";
+                if (choi == "e")
+                    choi = "E";
 
-            //for Hundred Place Value
-            Numb=Numb%1000;
-            PlaceValue = Numb / 100;
-            if(PlaceValue>=1 && PlaceValue<=9)
-            {
-                Console.Write(Numbers[PlaceValue - 1] + " Hundreds ");
-            }
-
-            //for place of two decimal
-            Numb=Numb%100;
-            if (Numb>0)
-                Console.Write(" And ");
-            
-            if(Numb<20 && Numb>0)
-                Console.WriteLine(Numbers[Numb-1]);
-            else
-            {
-                PlaceValue = Numb / 10;
-                if(PlaceValue>=2 && PlaceValue<=9)
+                while (choi != "C" && choi != "E")
                 {
-                    Console.Write(Tens[PlaceValue - 2]);
+                    Console.WriteLine("Invalid Input Re-Enter Your Choice");
+                    choi = Console.ReadLine();
+                    if (choi == "c")
+                        choi = "C";
+                    if (choi == "e")
+                        choi = "E";
                 }
-                Numb=Numb % 10;
-                if (Numb > 0 && Numb <= 9)
-                {
-                    Console.WriteLine(Numbers[Numb - 1]);
-                }
+                 
+
 
             }
-            Console.ReadLine();
         }
     }
 }
